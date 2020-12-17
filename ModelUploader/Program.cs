f588a9246152fd7edaad25b087c5d0ce61167c0c
Akshay Johar <ajohar@gmail.com>
2020-12-17 13:40:05 -0800

    Changing 2 Errors to Alerts/Warnings
    These should not be Errors, just Warnings

--- ModelUploader/Program.cs
+++ ModelUploader/Program.cs
@@ -209,7 +209,7 @@ namespace ModelUploader
                                 if (missingFile.Count<string>() > 1)
                                 {
                                     // More than one file was matched, log a warning
-                                    Log.Error("More than one file matched the prefix " + missingInterface + ".json in the ModelPath directory. Processing only the first");
+                                    Log.Alert("More than one file matched the prefix " + missingInterface + ".json in the ModelPath directory. Processing only the first");
                                 }
                                 // try to Upload the Model in the extends section
                                 exitProcess = UploadModel(missingFile[0]);
@@ -253,7 +253,7 @@ namespace ModelUploader
                     exitProcess = UploadModel(file);
                     if (exitProcess == true)
                     {
-                        Log.Error($"Could not find a definition for Interace {" + missingInterface + "}");
+                        Log.Alert($"Could not find a definition for Interace {" + missingInterface + "}");
                     }
                 }
                 else
